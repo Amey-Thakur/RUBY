9e8898422f2cb8476a17d75124b0bdf400e46bcc
msatmod <mega.modha@gmail.com>
2022-08-30 11:06:36 +0530

    Ruby

---
+++ Day 10/Include statement concept/Filly.rb
@@ -0,0 +1,11 @@
+module Week
+  First_Day = 'Sunday'
+
+  def Week.weeks_in_month
+    puts "You have 4 weeks in a month"
+  end
+
+  def Week.weeks_in_year
+    puts "You have 52 weeks in a year"
+  end
+end
