88bc3609c96b32de197fb92dfebeb5c89a025021
msatmod <mega.modha@gmail.com>
2022-08-30 11:05:15 +0530

    Ruby

---
+++ Day 6/For_loop.rb
@@ -0,0 +1,12 @@
+# Program for For Loop
+# No condition in For Loop
+
+=begin
+  for VARIABLE_NAME in RANGE
+    Code
+  end
+=end
+
+for i in 0..5
+  puts("value of local variable i = #{i}")
+end
---
+++ Day 6/Until_loop.rb
@@ -0,0 +1,15 @@
+# Program for Until Loop
+
+=begin
+  until condition do
+    Code
+  end
+=end
+
+$i = 0
+$num = 5
+
+until $i > $num do
+  puts ("Inside the loop $i = #$i");
+  $i += 1
+end
